ff83368544e973e85781555450bb5ad99b90e68a
Jono Brandel <jonobrandel@google.com>
2012-06-16 15:28:40 -0700

    updated point positions with vectors

--- sketches/HelloWorld.js
+++ sketches/HelloWorld.js
@@ -9,7 +9,7 @@ require([
     this.ctx.clearRect(0, 0, this.width, this.height);
     this.ctx.fillStyle = 'red';
     this.ctx.beginPath();
-    this.ctx.arc(this.width / 2, this.height / 2, 50, 0, Math.PI * 2, false);
+    this.ctx.arc(this.center.x, this.center.y, 50, 0, Math.PI * 2, false);
     this.ctx.fill();
   });
 
