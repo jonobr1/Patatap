dacce5baacf5b28c0b9749245fef93e779aba59e
jonobr1 <correspondence@jonobr1.com>
2014-02-17 16:49:47 -0800

    updated beginning

--- src/animations.js
+++ src/animations.js
@@ -1802,7 +1802,7 @@ window.animations = (function() {
       offset = Math.PI / 2;
       zigzag.rotation = Math.random() > 0.5 ? Math.PI : 0;
       x = 0;
-      zigzag.beginning = zigzag.ending = 0;
+      zigzag.ending = zigzag.beginning = 0;
 
       for (i = 0; i < amount; i++) {
         v = points[i];
@@ -1911,7 +1911,7 @@ window.animations = (function() {
       offset = Math.PI / 2;
       squiggle.rotation = Math.random() > 0.5 ? Math.PI : 0;
       // options.beginning = options.ending = 0;
-      squiggle.beginning = squiggle.ending = 0;
+      squiggle.ending = squiggle.beginning = 0;
       for (i = 0; i < amount; i++) {
         v = points[i];
         pct = i / amount;
