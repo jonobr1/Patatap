168a06de01c0c86a0e7e1b468801e99fe2620fcd
tonz <tonia_beglari@usjc.co.jp>
2021-10-21 15:42:17 -0700

    Playback Playhead: Display track progress and loop at the end.
    Uses requestAnimationFrame but might want to change this to our own step based on how we queue the notes.

--- src/drop-midi.js
+++ src/drop-midi.js
@@ -13,15 +13,18 @@ $(function() {
 
         const reader = new FileReader();
         reader.onload = function (e) {
-            const midi = new Midi(e.target.result);
-            currentMidiFile = midi;
-            console.log(currentMidiFile);
-            //Q: Call an autoplay function or will player loop always check for currentMidi?
+            $("#midi-title").html(file.name);
+            startFile(e.target.result);
         };
         reader.readAsArrayBuffer(file);
+    }
 
-        $("#midi-title").html(file.name);
+    function startFile(fileData) {
+        const midi = new Midi(fileData);
+        currentMidiFile = midi;
+        console.log(currentMidiFile);
         $nowPlaying.fadeIn();
+        playbackTime = 0.0;
     }
 
     function removeFile() {
@@ -29,7 +32,34 @@ $(function() {
         $nowPlaying.fadeOut(100);
         //Reset Var (This will stop the player)
         currentMidiFile = null;
+        playbackTime = 0.0;
     }
+
+    function updatePlayhead(deltaTime) {
+        if (!currentMidiFile) return;
+
+        playbackTime += (deltaTime * 0.001);
+        var fill = 0.0;
+        if (playbackTime > currentMidiFile.duration) {
+            playbackTime = 0.0;
+        } else {
+            fill = (playbackTime/currentMidiFile.duration) * 100;
+        }
+        $("#midi-track-progress").width(fill+"%");
+    }
+    
+    var tLastLoop = 0;
+    function playLoop(timestamp) {
+        if (currentMidiFile) {
+            var deltaTime = timestamp - tLastLoop;
+            updatePlayhead(deltaTime);
+            
+            tLastLoop = timestamp;
+        }
+        window.requestAnimationFrame(playLoop);
+    }
+    
+    window.requestAnimationFrame(playLoop);
 
     function dragOverHandler(ev) {
         // Prevent default behavior (Prevent file from being opened)
